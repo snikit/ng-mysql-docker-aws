32366015153f1f0507a18b8851a028de8743c248
Abhishek Thakur <snikitz@yahoo.com>
2017-09-29 14:00:03 +0530

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,10 +1,10 @@
 module.exports = {  
-    port: process.env.PORT || 8123,
+    port: process.env.PORT || 3000,
   db: {
     host: process.env.DATABASE_HOST || '127.0.0.1',
     database: 'users',
     user: 'local',
     password: 'local',
-    port: 3000
+    port: 3306
   }
 };
